592315b079d73187d829db72e4da7be4e4c7deba
YourName <arimran7315@gmail.com>
2026-09-23 22:27:08 +0500

    update the meta title

--- include/headerLinks.php
+++ include/headerLinks.php
@@ -1,44 +1,112 @@
 <head>
    <meta charset="utf-8">
-   <meta http-equiv="x-ua-compatible" content="ie=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+   <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+   <meta name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+   <!-- SEO Meta Tags -->
    <meta name="description"
-      content="Global Trading is a trusted textile and fabric trading company offering quality fabrics and textile products for businesses, retailers, and customers. Explore our products and connect with us for your fabric sourcing needs.">
+      content="Global Trading provides reliable industrial and trading solutions for businesses, offering quality products, professional services, and dependable supply solutions to meet diverse business needs.">
 
    <meta name="keywords"
-      content="Global Trading, textile trading, fabric trading, fabrics, textile products, fabric supplier, textile supplier, fabric wholesale, Pakistan textiles">
+      content="Global Trading, industrial solutions, trading solutions, industrial trading, trading company, industrial products, business solutions, supply solutions, industrial supplier, trading company Pakistan">
 
-   <meta name="author" content="Global Trading">
+   <meta name="author"
+      content="Global Trading">
 
-   <title>Global Trading | Textile & Fabric Trading Company</title>
+   <!-- Search Engine Instructions -->
+   <meta name="robots"
+      content="index, follow">
 
-   <link rel="apple-touch-icon" sizes="180x180" href="images/favicon_io/apple-touch-icon.png">
-   <link rel="icon" type="image/png" sizes="32x32" href="images/favicon_io/favicon-32x32.png">
-   <link rel="icon" type="image/png" sizes="16x16" href="images/favicon_io/favicon-16x16.png">
-   <link rel="manifest" href="images/favicon_io/site.webmanifest">
+   <!-- Website Title -->
+   <title>Global Trading | Industrial & Trading Solutions</title>
+
+   <!-- Canonical URL -->
+   <link rel="canonical"
+      href="https://globaltrading.live/">
+
+   <!-- Favicon -->
+   <link rel="apple-touch-icon"
+      sizes="180x180"
+      href="images/favicon_io/apple-touch-icon.png">
+
+   <link rel="icon"
+      type="image/png"
+      sizes="32x32"
+      href="images/favicon_io/favicon-32x32.png">
+
+   <link rel="icon"
+      type="image/png"
+      sizes="16x16"
+      href="images/favicon_io/favicon-16x16.png">
+
+   <link rel="manifest"
+      href="images/favicon_io/site.webmanifest">
+
+   <!-- Open Graph / Social Media -->
+   <meta property="og:type"
+      content="website">
+
+   <meta property="og:title"
+      content="Global Trading | Industrial & Trading Solutions">
+
+   <meta property="og:description"
+      content="Global Trading provides reliable industrial and trading solutions, offering quality products, professional services, and dependable supply solutions for businesses.">
+
+   <meta property="og:url"
+      content="https://globaltrading.live/">
+
+   <meta property="og:site_name"
+      content="Global Trading">
 
    <!-- Google Fonts -->
-   <link rel="preconnect" href="https://fonts.googleapis.com/">
-   <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
+   <link rel="preconnect"
+      href="https://fonts.googleapis.com">
+
+   <link rel="preconnect"
+      href="https://fonts.gstatic.com"
+      crossorigin>
 
    <link
       href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Sora:wght@100..800&display=swap"
       rel="stylesheet">
 
    <!-- CSS Files -->
-   <link rel="stylesheet" href="css/bootstrap.min.css">
-   <link rel="stylesheet" href="css/fancybox.css">
-   <link rel="stylesheet" href="css/swiper-bundle.min.css">
-   <link rel="stylesheet" href="css/all.min.css">
-   <link rel="stylesheet" href="css/jarallax.min.css">
-   <link rel="stylesheet" href="css/nice-select.css">
-   <link rel="stylesheet" href="css/animate.css">
-
-   <!-- Style CSS -->
-   <link rel="stylesheet" href="css/style.css">
+   <link rel="stylesheet"
+      href="css/bootstrap.min.css">
 
    <link rel="stylesheet"
+      href="css/fancybox.css">
+
+   <link rel="stylesheet"
+      href="css/swiper-bundle.min.css">
+
+   <link rel="stylesheet"
+      href="css/all.min.css">
+
+   <link rel="stylesheet"
+      href="css/jarallax.min.css">
+
+   <link rel="stylesheet"
+      href="css/nice-select.css">
+
+   <link rel="stylesheet"
+      href="css/animate.css">
+
+   <!-- Main Style CSS -->
+   <link rel="stylesheet"
+      href="css/style.css">
+
+   <!-- Font Awesome -->
+   <link
+      rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+
+   <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+   <![endif]-->
 
 </head>
