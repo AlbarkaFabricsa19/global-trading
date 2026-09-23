bdbc7bf04c0cf8926a491a50099b798410f90331
YourName <arimran7315@gmail.com>
2026-09-23 22:25:29 +0500

    update seo tags

--- include/headerLinks.php
+++ include/headerLinks.php
@@ -2,23 +2,30 @@
    <meta charset="utf-8">
    <meta http-equiv="x-ua-compatible" content="ie=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
    <meta name="description"
       content="Global Trading is a trusted textile and fabric trading company offering quality fabrics and textile products for businesses, retailers, and customers. Explore our products and connect with us for your fabric sourcing needs.">
 
-<meta name="keywords"
-      content="Global Trading, textile trading, fabric trading, fabrics, textile products, quality fabrics, fabric supplier, textile supplier, fabric wholesale, Pakistan textiles">
-   <meta name="author" content="theme-village">
-   <title>Global Trading - Your Trusted Business Partners</title>
+   <meta name="keywords"
+      content="Global Trading, textile trading, fabric trading, fabrics, textile products, fabric supplier, textile supplier, fabric wholesale, Pakistan textiles">
+
+   <meta name="author" content="Global Trading">
+
+   <title>Global Trading | Textile & Fabric Trading Company</title>
+
    <link rel="apple-touch-icon" sizes="180x180" href="images/favicon_io/apple-touch-icon.png">
    <link rel="icon" type="image/png" sizes="32x32" href="images/favicon_io/favicon-32x32.png">
    <link rel="icon" type="image/png" sizes="16x16" href="images/favicon_io/favicon-16x16.png">
    <link rel="manifest" href="images/favicon_io/site.webmanifest">
+
    <!-- Google Fonts -->
    <link rel="preconnect" href="https://fonts.googleapis.com/">
    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
+
    <link
-      href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&amp;family=Sora:wght@100..800&amp;display=swap"
+      href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Sora:wght@100..800&display=swap"
       rel="stylesheet">
+
    <!-- CSS Files -->
    <link rel="stylesheet" href="css/bootstrap.min.css">
    <link rel="stylesheet" href="css/fancybox.css">
@@ -27,11 +34,11 @@
    <link rel="stylesheet" href="css/jarallax.min.css">
    <link rel="stylesheet" href="css/nice-select.css">
    <link rel="stylesheet" href="css/animate.css">
+
    <!-- Style CSS -->
    <link rel="stylesheet" href="css/style.css">
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
-   <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
+
+   <link rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+
 </head>
